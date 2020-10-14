201f978a790a9c866c8ab11ada6b69866e5e6da4
Kevin <anmoron11@gmail.com>
2020-10-14 19:40:38 -0400

    Added BS JS, SC, and FA

--- _data/stickers.js
+++ _data/stickers.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const fetch = require('node-fetch');
 
 module.exports = () => {
   
   return fetch('https://api.airtable.com/v0/appN16FUXQMKEFXMG/Sticker%20Info', {
     headers: {
-      'Authorization': 'Bearer keyx8ptYtJr13jjs6'
+      'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`
     }
   })
   .then(response => response.json())
